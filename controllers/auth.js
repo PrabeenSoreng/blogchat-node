5f7fa37592f0ebd01e091cd7800d5a01b1ffdb7d
Prabeen <prabeensoreng2119@gmail.com>
2019-05-07 15:45:27 +0530

    jwt added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/env");
 const { validationResult } = require("express-validator/check");
 
 exports.signup = async (req, res, next) => {
@@ -21,9 +23,14 @@ exports.signup = async (req, res, next) => {
       password: hashPwd
     });
     await user.save();
-
+    const token = jwt.sign(
+      { username: user.username, email: user.email },
+      config.JSON_SECRET,
+      { expiresIn: "1h" }
+    );
     res.status(201).json({
-      message: "Signup successful!"
+      message: "Signup successful!",
+      token
     });
   } catch (err) {
     if (!err.statusCode) err.statusCode = 500;
